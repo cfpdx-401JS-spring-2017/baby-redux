1471b201fdc9b6e5b84545fcf5a42180152ac59b
Morgan Fogarty <mofo37@gmail.com>
2017-06-23 15:37:24 -0700

    change container to properly map state to props.

--- app/src/salutation/container.js
+++ app/src/salutation/container.js
@@ -3,11 +3,7 @@ import { bindActionCreators } from 'redux';
 import * as actions from './actions';
 import Salutation from './Salutation';
 
-function mapStateToProps(state) {
-  return {
-    Salutation: state
-  };
-}
+const mapStateToProps = (state) => state;
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(actions, dispatch);
